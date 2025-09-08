6a103167ba0f137c6447b4c9dbdfc614647fbcf6
RHOHYUNSOO <ssanta011205@skuniv.ac.kr>
2025-09-08 10:18:11 +0900

    [level 0] Title: 평행, Time: 0.00 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,17 @@
+def solution(dots):
+    answer = 0
+    x1=dots[0][0]
+    x2=dots[1][0]
+    
+    y1=dots[0][1]
+    y2=dots[1][1]
+    
+    x3=dots[2][0]
+    x4=dots[3][0]
+    
+    y3=dots[2][1]
+    y4=dots[3][1]
+    
+    if (x1-x2)/(y1-y2)==(x3-x4)/(y3-y4) or (x1-x3)/(y1-y3)==(x2-x4)/(y2-y4) or (x1-x4)/(y1-y4)==(x2-x3)/(y2-y3):
+        return 1
+    return 0
