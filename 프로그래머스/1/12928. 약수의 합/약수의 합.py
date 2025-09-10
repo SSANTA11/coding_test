435abb2be23cdf8e34fd9d6a2f3b6ec56c54504c
RHOHYUNSOO <ssanta011205@skuniv.ac.kr>
2025-09-10 10:58:59 +0900

    [level 1] Title: 약수의 합, Time: 0.12 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,6 @@
 def solution(n):
     answer=0
-    for i in range(1,n+1):
+    for i in range(1,(n//2)+1):
         if n%i==0:
             answer+=i
-            print(answer)
-    return answer
+    return answer+n
