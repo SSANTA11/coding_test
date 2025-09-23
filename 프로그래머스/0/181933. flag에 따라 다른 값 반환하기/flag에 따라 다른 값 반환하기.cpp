0090e13fd8dfb5e3f5e8f4e6c8fa7a22bf987939
RHOHYUNSOO <ssanta011205@skuniv.ac.kr>
2025-09-23 11:33:30 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, bool flag) {
+    if (flag==1){
+        return a+b;
+    }
+    else
+        return a-b;
+}
